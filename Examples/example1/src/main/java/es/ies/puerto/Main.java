211f5aeeea5cc7426dfda3451f7f795af0ae7baa
eduglezexp <eduex04@gmail.com>
2024-09-13 20:28:11 +0100

    uploading example 1

--- Examples/example1/src/main/java/es/ies/puerto/Main.java
+++ Examples/example1/src/main/java/es/ies/puerto/Main.java
@@ -1,7 +1,15 @@
 package es.ies.puerto;
 
 public class Main {
+
+    static int edad;
+
     public static void main(String[] args) {
+        int suma = 2 + 3;
+        int resta = 3 - 2;
         System.out.println("Hello world!");
+        System.out.println(edad);
+        System.out.println("Mi suma es " +suma);
+        System.out.println("Mi resta es " +resta);
     }
 }
