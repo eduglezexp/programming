387c34aea117fd8fda2fe03e2de240c26bde0c6d
eduglezexp <eduex04@gmail.com>
2024-09-27 01:26:02 +0100

    tarea3 completed

--- Homeworks/tarea3/src/main/java/es/ies/puerto/Ejercicio14.java
+++ Homeworks/tarea3/src/main/java/es/ies/puerto/Ejercicio14.java
@@ -12,56 +12,71 @@ import java.util.Scanner;
 
 public class Ejercicio14 {
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Introduce el tipo de pokemon del jugador 1: " +
-        "(Agua, Fuego, Planta)");
-        String tipo1 = scanner.nextLine();
-        tipo1 = tipo1.toLowerCase();
-        System.out.println("Introduce el tipo de pokemon del jugador 2: " + 
-        "(Agua, Fuego, Planta)");
-        String tipo2 = scanner.nextLine();
-        tipo2 = tipo2.toLowerCase();
-        
+        System.out.println("Introduce el tipo de pokemon del jugador 1: (Agua, Fuego, Planta)");
+        String tipo1 = scanner.nextLine().toLowerCase(); // Convertir a minúsculas para evitar errores
+        System.out.println("Introduce el tipo de pokemon del jugador 2: (Agua, Fuego, Planta)");
+        String tipo2 = scanner.nextLine().toLowerCase();
+
+        // Switch para determinar el resultado según el tipo del jugador 1
         switch (tipo1) {
             case "agua":
                 switch (tipo2) {
                     case "fuego":
                         System.out.println("Gano el jugador 1");
+                        break;
                     case "planta":
                         System.out.println("Gano el jugador 2");
+                        break;
                     case "agua":
                         System.out.println("Empatasteis");
+                        break;
                     default:
                         System.out.println("El tipo de pokemon del jugador 2 es invalido");
+                        break;
                 }
-            
+                break;
+
             case "fuego":
                 switch (tipo2) {
                     case "planta":
                         System.out.println("Gano el jugador 1");
+                        break;
                     case "agua":
                         System.out.println("Gano el jugador 2");
+                        break;
                     case "fuego":
                         System.out.println("Empatasteis");
+                        break;
                     default:
                         System.out.println("El tipo de pokemon del jugador 2 es invalido");
+                        break;
                 }
+                break;
 
             case "planta":
                 switch (tipo2) {
                     case "agua":
                         System.out.println("Gano el jugador 1");
+                        break;
                     case "fuego":
                         System.out.println("Gano el jugador 2");
+                        break;
                     case "planta":
                         System.out.println("Empatasteis");
+                        break;
                     default:
                         System.out.println("El tipo de pokemon del jugador 2 es invalido");
+                        break;
                 }
+                break;
 
             default:
                 System.out.println("El tipo de pokemon del jugador 1 es invalido");
+                break;
         }
+
         scanner.close();
     }
 }
