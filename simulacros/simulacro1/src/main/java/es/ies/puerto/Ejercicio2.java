5969edc3016bf3f05aa337bd558310fd35c4863e
eduglezexp <eduex04@gmail.com>
2024-10-21 22:47:51 +0100

    simulacro1 done

--- simulacros/simulacro1/src/main/java/es/ies/puerto/Ejercicio2.java
+++ simulacros/simulacro1/src/main/java/es/ies/puerto/Ejercicio2.java
@@ -11,18 +11,27 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         int[] numeros = {1, 2, 2, 3, 3, 3, 4};
         System.out.println(contarNumerosRepetidos(numeros));
+        int[] numeros2 = {1, 2, 2, 3, 3, 3, 4};
+        System.out.println(contarNumerosRepetidos(numeros2));
+        int[] numeros3 = {7, 8, 9, 7, 8, 9, 7, 9};
+        System.out.println(contarNumerosRepetidos(numeros3));
     }
 
     public static String contarNumerosRepetidos(int[] numeros) {
-        int contador = 0;
+        boolean[] contado = new boolean[numeros.length];
+        String resultado = "";
         for (int i = 0; i < numeros.length; i++) {
-            for (int j = 0; j < numeros.length; j++) {
-                if (numeros[i] == numeros[j]) {
-                    contador++;
-                    return numeros[i]+ " aparece " +contador+ " veces";
-                }
+            if (!contado[i]) {
+                int contador = 1;
+                for (int j = i + 1; j < numeros.length; j++) {
+                    if (numeros[i] == numeros[j]) {
+                        contador++;
+                        contado[j] = true;
+                    }
+                } 
+                resultado += numeros[i]+ " aparece " +contador+ " veces\n";   
             }
         }
-        return "";
+        return resultado;
     }
 }
